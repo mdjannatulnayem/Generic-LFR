ceff317ae1bf76c20c5b219452025f80d16e8b85
Md. Jannatul Nayem <64035356+mdjannatulnayem@users.noreply.github.com>
2022-11-20 01:12:06 +0600

    Update GenericLFR.ino

--- GenericLFR/GenericLFR.ino
+++ GenericLFR/GenericLFR.ino
@@ -11,9 +11,10 @@
 #define ir5 3
 #define ir6 2
 
-int irValues[7],STOP = 0;
+int irValues[7],STOP = 5;
 
-void setup() {
+void setup() 
+{
   Serial.begin(9600);
   pinMode(leftmS,OUTPUT);
   pinMode(rightmS,OUTPUT);
@@ -29,10 +30,12 @@ void setup() {
   pinMode(ir6,INPUT);
 }
 
-void loop() {
-  readSensor();
-  actuateMotor();
-  Serial.println();
+void loop()
+{
+  readSensor(); /*Reads from IR sensors*/
+  printreading(); /*Print out the readings*/
+  actuateMotor(); /*Run motor*/
+  Serial.println(); /*Newline*/
 }
 
 void readSensor(){
@@ -42,6 +45,9 @@ void readSensor(){
   irValues[4] = digitalRead(ir4);
   irValues[5] = digitalRead(ir5);
   irValues[6] = digitalRead(ir6);
+}
+
+void printreading(){
   Serial.print(
     String(irValues[1])
     +String(irValues[2])
@@ -49,56 +55,68 @@ void readSensor(){
     +String(irValues[4])
     +String(irValues[5])
     +String(irValues[6])
-    +">>");
+    +">>"
+    +String(STOP)
+    +">>"
+  ); //Prints out the readings!
 }
 
 void actuateMotor()
 {
-       if(irValues[1] == 1 && irValues[2] == 1 && irValues[3] == 1 && irValues[4] == 1 && irValues[5] == 0 && irValues[6] == 0) //1 ~ 111100
-          left();   //1
-  else if(irValues[1] == 0 && irValues[2] == 0 && irValues[3] == 1 && irValues[4] == 1 && irValues[5] == 1 && irValues[6] == 1) //2 ~ 001111
+  //Group of 4
+  if(irValues[1] == 1 && irValues[2] == 1 && irValues[3] == 1 && irValues[4] == 1 && irValues[5] == 0 && irValues[6] == 0) //1 - 111100
+    left();         //1
+  if(irValues[1] == 0 && irValues[2] == 0 && irValues[3] == 1 && irValues[4] == 1 && irValues[5] == 1 && irValues[6] == 1) //2 - 001111
     right();        //2
-  else if(irValues[1] == 0 && irValues[2] == 1 && irValues[3] == 1 && irValues[4] == 1 && irValues[5] == 0 && irValues[6] == 0) //3 ~ 011100
+  //Group of 3
+  if(irValues[1] == 0 && irValues[2] == 1 && irValues[3] == 1 && irValues[4] == 1 && irValues[5] == 0 && irValues[6] == 0) //3 - 011100
     left();         //3
-  else if(irValues[1] == 0 && irValues[2] == 0 && irValues[3] == 1 && irValues[4] == 1 && irValues[5] == 1 && irValues[6] == 0) //4 ~ 001110
+  if(irValues[1] == 0 && irValues[2] == 0 && irValues[3] == 1 && irValues[4] == 1 && irValues[5] == 1 && irValues[6] == 0) //4 - 001110
     right();        //4
-  else if(irValues[1] == 1 && irValues[2] == 1 && irValues[3] == 1 && irValues[4] == 0 && irValues[5] == 0 && irValues[6] == 0) //5 ~ 111000
+  if(irValues[1] == 1 && irValues[2] == 1 && irValues[3] == 1 && irValues[4] == 0 && irValues[5] == 0 && irValues[6] == 0) //5 - 111000
     left();         //5
-  else if(irValues[1] == 0 && irValues[2] == 0 && irValues[3] == 0 && irValues[4] == 1 && irValues[5] == 1 && irValues[6] == 1) //6 ~ 000111
+  if(irValues[1] == 0 && irValues[2] == 0 && irValues[3] == 0 && irValues[4] == 1 && irValues[5] == 1 && irValues[6] == 1) //6 - 000111
     right();        //6
-  else if(irValues[1] == 0 && irValues[2] == 1 && irValues[3] == 1 && irValues[4] == 0 && irValues[5] == 0 && irValues[6] == 0) //7 ~ 011000
+  //Group of 2
+  if(irValues[1] == 0 && irValues[2] == 1 && irValues[3] == 1 && irValues[4] == 0 && irValues[5] == 0 && irValues[6] == 0) //7 - 011000
     left();         //7
-  else if(irValues[1] == 1 && irValues[2] == 1 && irValues[3] == 0 && irValues[4] == 0 && irValues[5] == 0 && irValues[6] == 0) //8 ~ 110000
+  if(irValues[1] == 1 && irValues[2] == 1 && irValues[3] == 0 && irValues[4] == 0 && irValues[5] == 0 && irValues[6] == 0) //8 - 110000
     left();         //8
-  else if(irValues[1] == 0 && irValues[2] == 0 && irValues[3] == 0 && irValues[4] == 1 && irValues[5] == 1 && irValues[6] == 0) //9 ~ 000110
+  if(irValues[1] == 0 && irValues[2] == 0 && irValues[3] == 0 && irValues[4] == 1 && irValues[5] == 1 && irValues[6] == 0) //9 - 000110
     right();        //9
-  else if(irValues[1] == 0 && irValues[2] == 0 && irValues[3] == 0 && irValues[4] == 0 && irValues[5] == 1 && irValues[6] == 1) //10 ~ 000011
+  if(irValues[1] == 0 && irValues[2] == 0 && irValues[3] == 0 && irValues[4] == 0 && irValues[5] == 1 && irValues[6] == 1) //10 - 000011
     right();        //10
-  else if(irValues[1] == 0 && irValues[2] == 0 && irValues[3] == 1 && irValues[4] == 1 && irValues[5] == 0 && irValues[6] == 0) //11 ~ 001100
+  if(irValues[1] == 0 && irValues[2] == 0 && irValues[3] == 1 && irValues[4] == 1 && irValues[5] == 0 && irValues[6] == 0) //11 - 001100
     forward();      //11
-  else if(irValues[1] == 0 && irValues[2] == 0 && irValues[3] == 1 && irValues[4] == 0 && irValues[5] == 0 && irValues[6] == 0) //12 ~ 001000
+  //Group of 1
+  if(irValues[1] == 0 && irValues[2] == 0 && irValues[3] == 1 && irValues[4] == 0 && irValues[5] == 0 && irValues[6] == 0) //12 - 001000
     forward();      //12
-  else if(irValues[1] == 0 && irValues[2] == 1 && irValues[3] == 0 && irValues[4] == 0 && irValues[5] == 0 && irValues[6] == 0) //13 ~ 010000 
+  if(irValues[1] == 0 && irValues[2] == 1 && irValues[3] == 0 && irValues[4] == 0 && irValues[5] == 0 && irValues[6] == 0) //13 - 010000 
     left();         //13
-  else if(irValues[1] == 1 && irValues[2] == 0 && irValues[3] == 0 && irValues[4] == 0 && irValues[5] == 0 && irValues[6] == 0) //14 ~ 100000
+  if(irValues[1] == 1 && irValues[2] == 0 && irValues[3] == 0 && irValues[4] == 0 && irValues[5] == 0 && irValues[6] == 0) //14 - 100000
     left();         //14
-  else if(irValues[1] == 0 && irValues[2] == 0 && irValues[3] == 0 && irValues[4] == 1 && irValues[5] == 0 && irValues[6] == 0) //15 ~ 000100
+  if(irValues[1] == 0 && irValues[2] == 0 && irValues[3] == 0 && irValues[4] == 1 && irValues[5] == 0 && irValues[6] == 0) //15 - 000100
     forward();      //15
-  else if(irValues[1] == 0 && irValues[2] == 0 && irValues[3] == 0 && irValues[4] == 0 && irValues[5] == 1 && irValues[6] == 0) //16 ~ 000010
-    right();        //16
-  else if(irValues[1] == 0 && irValues[2] == 0 && irValues[3] == 0 && irValues[4] == 0 && irValues[5] == 0 && irValues[6] == 1) //17 ~ 000001
+  if(irValues[1] == 0 && irValues[2] == 0 && irValues[3] == 0 && irValues[4] == 0 && irValues[5] == 1 && irValues[6] == 0) //16 - 000010
+    right();        //1
+  if(irValues[1] == 0 && irValues[2] == 0 && irValues[3] == 0 && irValues[4] == 0 && irValues[5] == 0 && irValues[6] == 1) //17 - 000001
     right();        //17
-  else if(irValues[1] == 1 && irValues[2] == 1 && irValues[3] == 1 && irValues[4] == 1 && irValues[5] == 1 && irValues[6] == 1) //18 ~ 111111
+  //All of 1
+  if(irValues[1] == 1 && irValues[2] == 1 && irValues[3] == 1 && irValues[4] == 1 && irValues[5] == 1 && irValues[6] == 1) //18 - 111111
   {
-    delay(200);
-    STOP += 1;
-    if(STOP > 5){
-      stop();
-      STOP = 0;
+    if(STOP > 0){
+      forward();
+      delayMicroseconds(100000);
+      readSensor();
+      if(irValues[1] == 0 || irValues[2] == 0 || irValues[3] == 0 || irValues[4] == 0 || irValues[5] == 0 || irValues[6] == 0) {
+        STOP -= 1;
+      }
+    }
+    else{
+      fullstop();
     }
   }
-  else
-    search();
+  else{Serial.print("LOST-TRAIL!");}
 }
 
 void forward(){
@@ -109,16 +127,6 @@ void forward(){
   digitalWrite(leftmN,LOW);
   digitalWrite(rightmP,HIGH);
   digitalWrite(rightmN,LOW);  
-}
-
-void back(){
-  Serial.print("BACK");
-  analogWrite(leftmS,160);
-  analogWrite(rightmS,160); 
-  digitalWrite(leftmP,LOW);
-  digitalWrite(leftmN,HIGH);
-  digitalWrite(rightmP,LOW);
-  digitalWrite(rightmN,HIGH);  
 }
 
 void left(){
@@ -141,7 +149,7 @@ void right(){
   digitalWrite(rightmN,LOW);
 }
 
-void stop(){
+void fullstop(){
   Serial.print("STOP");
   analogWrite(leftmS,0);
   analogWrite(rightmS,0);  
@@ -149,9 +157,4 @@ void stop(){
   digitalWrite(leftmN,LOW);
   digitalWrite(rightmP,LOW);
   digitalWrite(rightmN,LOW);
-}
-
-void search(){
-  Serial.print("SEARCH");
-  /* NOT IMPLEMENTED YET*/
 }
